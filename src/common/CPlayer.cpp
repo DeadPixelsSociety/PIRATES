16264ced798c094e71716168a858f5d15de58e6f
Romain Mekarni <romain.mekarni@gmail.com>
2014-11-12 13:10:14 +0100

    add Player class

---
+++ src/common/CPlayer.cpp
@@ -0,0 +1,23 @@
+//
+//  @ Project :   PIRATES
+//  @ File NAme : CPlayer.cpp
+//  @ Date :      12/11/2014
+//  @ Author :    Romain
+//
+
+
+#include "CPlayer.h"
+
+
+CPlayer::CPlayer() :
+m_sName("Player"),
+m_vPosition(1, 1)
+{
+  
+}
+
+CPlayer::~CPlayer()
+{
+  
+}
+
